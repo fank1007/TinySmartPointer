a48565eba1e6ac22f08569b1435345ea07a99893
fk <3445777927@qq.com>
2023-01-04 23:33:52 +0800

    commit

--- source/shared_ptr/shared_ptr.h
+++ source/shared_ptr/shared_ptr.h
@@ -2,17 +2,40 @@
 #define SHARED_PTR_H_
 
 #include<utility>
+#include<mutex>
 
 namespace cpp{
 
+
+/**
+ * @brief 引用计数智能指针
+ * 
+ * 通过引用计数对资源进行管理，在离开作用域时，如果计数为零则释放资源
+ * 
+ * @tparam T {存放的指针类型}
+ */
 template<typename T>
 class SharedPointer{
  public:
-
+  /**
+   * @brief 默认构造函数
+   * 
+   */
   SharedPointer() : pointer_(nullptr),user_count_(nullptr) {}
-
+  /**
+   * @brief 构造函数
+   * 
+   * @param pointer {T*}   智能指针内存放的资源
+   */
   explicit SharedPointer(T* pointer) : pointer_(pointer),user_count_(new int(1)) {}
 
+  /**
+   * @brief 拷贝构造函数
+   * 
+   * 浅拷贝一个智能指针，在每次拷贝时使计数增加1；
+   * 
+   * @param copy {const SharedPointer&} 被拷贝的对象
+   */
   SharedPointer(const SharedPointer& copy)
       :pointer_(copy.pointer_),
        user_count_(copy.user_count_) {
@@ -21,13 +44,26 @@ class SharedPointer{
     }
   }
 
+
+  /**
+   * @brief 移动构造函数
+   * 
+   * 转移所有权
+   * 
+   * @param move {SharedPointer&&} 被移动的对象
+   */
   SharedPointer(SharedPointer&& move)
       :pointer_(move.pointer_),
        user_count_(move.user_count_) {
     move.pointer_ = nullptr;
     move.user_count_ = nullptr;
   }
-
+  /**
+   * @brief 析构函数
+   * 
+   * 销毁一个智能指针，将计数减1，在计数等于0时，释放内存
+   * 
+   */
   ~SharedPointer(){
     if (user_count_ != nullptr) {
       --(*user_count_);
@@ -39,8 +75,17 @@ class SharedPointer{
      user_count_ = nullptr;
     }
   }
-
+  /**
+   * @brief 拷贝赋值运算符
+   * 
+   * 浅拷贝一个智能指针，并将计数加1
+   * 在进行自我赋值时不做任何事
+   * 
+   * @param copy {const SharedPointer&} 被拷贝的对象
+   * @return SharedPointer& 返回 *this
+   */
   SharedPointer& operator=(const SharedPointer& copy) {
+    std::unique_lock<std::mutex> lock(mutex_, std::try_to_lock);
     if (&copy != this) {
       pointer_ = copy.pointer_;
       user_count_ = copy.user_count_;
@@ -50,7 +95,18 @@ class SharedPointer{
     return *this;
   }
 
+
+  /**
+   * @brief 移动赋值运算符
+   * 
+   * 转移所有权
+   * 在自我赋值时不做任何事
+   * 
+   * @param move {SharedPointer&&} 被移动的对象
+   * @return SharedPointer&  返回 *this
+   */
   SharedPointer& operator=(SharedPointer&& move) {
+    std::unique_lock<std::mutex> lock(mutex_, std::try_to_lock);
     if (&move != this) {
       pointer_ = move.pointer_;
       user_count_ = move.user_count_;
@@ -61,14 +117,31 @@ class SharedPointer{
     return *this;
   }
 
+
+  /**
+   * @brief 返回存放的指针
+   * 
+   * @return T* 返回存放的指针
+   */
   T* pointer() {
    return pointer_;
   }
 
+
+  /**
+   * @brief 返回当前指针的计数
+   * 
+   * @return int 返回当前指针的计数
+   */
   int user_count() {
    return (*user_count_);
   }
-
+  /**
+   * @brief 查看是否只有一个智能指针管理该资源
+   * 
+   * @return true 只有一个对象管理该资源
+   * @return false 反之
+   */
   bool Unique() {
     if (*user_count_ != 1 || user_count_ == nullptr) {
       return false;
@@ -76,15 +149,31 @@ class SharedPointer{
       return true;
     }
   }
-
+  /**
+   * @brief 返回指针指向的对象
+   * 
+   * @return T 指向的对象
+   */
   T operator*() {
     return *pointer_;
   }
 
+
+  /**
+   * @brief 访问类成员
+   * 
+   * @return T* 
+   */
   T* operator->() {
     return pointer_;
   }
 
+  /**
+   * @brief 判断该指针是否为空
+   * 
+   * @return true 该指针为空
+   * @return false 反之
+   */
   explicit operator bool() {
     if (pointer_ == nullptr) {
       return false;
@@ -92,13 +181,25 @@ class SharedPointer{
       return true;
     }
   }
-
+  /**
+   * @brief 重新设置指针
+   * 
+   * 在计数为1的时候释放原资源，否则计数减1
+   * 
+   */
   void Reset(T*);
  private:
   T* pointer_;
   int* user_count_;
+  std::mutex mutex_;
 };
 
+
+/**
+ * @brief 类模板特化用于处理数组类型
+ * 
+ * @tparam T 
+ */
 template<typename T>
 class SharedPointer<T[]>{
  public:
@@ -134,6 +235,7 @@ class SharedPointer<T[]>{
   }
 
   SharedPointer& operator=(const SharedPointer& copy) {
+    std::unique_lock<std::mutex> lock(mutex_, std::try_to_lock);
     if (&copy != this) {
       pointer_ = copy.pointer_;
       user_count_ = copy.user_count_;
@@ -144,6 +246,7 @@ class SharedPointer<T[]>{
   }
 
   SharedPointer& operator=(SharedPointer&& move) {
+    std::unique_lock<std::mutex> lock(mutex_, std::try_to_lock);
     if (&move != this) {
       pointer_ = move.pointer_;
       user_count_ = move.user_count_;
@@ -195,11 +298,13 @@ class SharedPointer<T[]>{
  private:
   T* pointer_;
   int* user_count_;
+  std::mutex mutex_;
 };
 
 
 template<typename T>
 void SharedPointer<T>::Reset(T* pointer) {
+  std::unique_lock<std::mutex> lock(mutex_, std::try_to_lock);
   if (*user_count_ == 1) {
     auto p = pointer_;
     pointer_ = pointer;
@@ -216,6 +321,7 @@ void SharedPointer<T>::Reset(T* pointer) {
 
 template<typename T>
 void SharedPointer<T[]>::Reset(T* pointer) {
+  std::unique_lock<std::mutex> lock(mutex_, std::try_to_lock);
   if (*user_count_ == 1) {
     auto p = pointer_;
     pointer_ = pointer;
@@ -230,11 +336,28 @@ void SharedPointer<T[]>::Reset(T* pointer) {
   }
 }
 
+
+/**
+ * @brief 构造一个智能指针并返回
+ * 
+ * @tparam T 智能指针存放指针的类型
+ * @tparam Args 参数包
+ * @param args 参数包
+ * @return SharedPointer<T> 返回一个智能指针
+ */
 template<typename T, typename... Args>
 SharedPointer<T> MakeShared(Args&&... args) {
   return std::move(SharedPointer<T>(new T(std::forward<Args>(args)...)));
 }
 
+
+/**
+ * @brief 构造一个智能指针并返回
+ * 
+ * @tparam T 智能指针存放指针的类型
+ * @param size 数组的大小
+ * @return SharedPointer<T[]> 返回一个智能指针
+ */
 template<typename T>
 SharedPointer<T[]> MakeShared(int size) {
   return std::move(SharedPointer<T[]>(new T[size]));
--- source/unique_ptr/unique_ptr.h
+++ source/unique_ptr/unique_ptr.h
@@ -2,42 +2,124 @@
 #define UNIQUE_PTR_H_
 
 #include<utility>
+#include<mutex>
 
 namespace cpp{
 
+
+/**
+ * @brief 独占智能指针
+ * 
+ * 每次只能有一个智能指针持有资源，无法拷贝，只能移动
+ * 
+ * @tparam T 存放指针的类型
+ */
 template<typename T>
 class UniquePointer{
  public:
+
+  /**
+   * @brief 默认构造函数
+   * 
+   */
   UniquePointer() : pointer_(nullptr) {}
+
+
+  /**
+   * @brief 构造函数
+   * 
+   * @param pointer 存入智能指针的资源
+   */
   explicit UniquePointer(T* pointer) : pointer_(pointer) {}
+
+
+  /**
+   * @brief 删除的拷贝构造函数
+   * 
+   * @param copy 
+   */
   UniquePointer(const UniquePointer& copy) = delete;
+
+
+  /**
+   * @brief 移动构造函数
+   * 
+   * 转移所有权
+   * 
+   * @param move {Unique&&} 被移动的对象
+   */
   UniquePointer(UniquePointer&& move) : pointer_(move.pointer_) {
     move.pointer_ = nullptr;
   }
+
   ~UniquePointer() {
     delete pointer_;
     pointer_ = nullptr;
   }
+
+
+  /**
+   * @brief 删除的拷贝赋值运算符
+   * 
+   * @param copy 
+   * @return UniquePointer& 
+   */
   UniquePointer& operator=(const UniquePointer& copy) = delete;
+
+
+  /**
+   * @brief 移动赋值运算符
+   * 
+   *转移所有权 
+   *
+   * @param move {Unique&&} 被移动的对象
+   * @return UniquePointer& 返回 *this
+   */
   UniquePointer& operator=(UniquePointer&& move) {
+    std::unique_lock<std::mutex> lock(mutex_, std::try_to_lock);
     if (&move != this) {
       pointer_ = move.pointer_;
       move.pointer_ = nullptr;
     }
   }
 
+
+  /**
+   * @brief 返回存放的指针
+   * 
+   * @return T* 返回存放的指针
+   */
   T operator*() {
     return *pointer_;
   }
 
+
+  /**
+   * @brief 访问类成员
+   * 
+   * @return T* 
+   */
   T* operator->() {
     return pointer_;
   }
 
+
+  /**
+   * @brief 返回存放的指针
+   * 
+   * @return T* 返回存放的指针
+   */
   T* pointer() {
     return pointer_;
   }
 
+
+  /**
+   * @brief 判断该指针是否为空
+   * 
+   * @return true 该指针为空
+   * @return false 反之
+   */
   explicit operator bool() {
     if (pointer_ == nullptr) {
       return false;
@@ -46,21 +128,39 @@ class UniquePointer{
     }
   }
   
+  /**
+   * @brief 销毁原资源，存入新资源
+   * 
+   * @param pointer 存入的新资源
+   */
   void Reset(T* pointer) {
+    std::unique_lock<std::mutex> lock(mutex_, std::try_to_lock);
     auto p = pointer_;
     pointer_ = pointer;
     delete p;
   }
+
  private:
   T* pointer_;
+  std::mutex mutex_;
 };
 
+
+/**
+ * @brief 类模板特化用于处理数组类型
+ * 
+ * @tparam T 
+ */
 template<typename T>
 class UniquePointer<T[]>{
  public:
+
   UniquePointer() : pointer_(nullptr) {}
+
   explicit UniquePointer(T* pointer) : pointer_(pointer) {}
+
   UniquePointer(const UniquePointer& copy) = delete;
+
   UniquePointer(UniquePointer&& move) : pointer_(move.pointer_) {
     move.pointer_ = nullptr;
   }
@@ -68,8 +168,11 @@ class UniquePointer<T[]>{
     delete[] pointer_;
     pointer_ = nullptr;
   }
+
   UniquePointer& operator=(const UniquePointer& copy) = delete;
+
   UniquePointer& operator=(UniquePointer&& move) {
+    std::unique_lock<std::mutex> lock(mutex_, std::try_to_lock);
     if (&move != this) {
       pointer_ = move.pointer_;
       move.pointer_ = nullptr;
@@ -101,19 +204,40 @@ class UniquePointer<T[]>{
   }
   
   void Reset(T* pointer) {
+    std::unique_lock<std::mutex> lock(mutex_, std::try_to_lock);
     auto p = pointer_;
     pointer_ = pointer;
     delete[] p;
   }
+
  private:
   T* pointer_;
+
 };
 
+
+/**
+ * @brief 构造一个智能指针并返回
+ * 
+ * @tparam T 智能指针存放指针的类型
+ * @tparam Args 参数包
+ * @param args 参数包
+ * @return UniquePointer<T> 返回一个智能指针
+ */
 template<typename T, typename... Args>
 UniquePointer<T> MakeUnique(Args&&... args) {
   return std::move(UniquePointer<T>(new T(std::forward<Args>(args)...)));
 }
 
+
+
+/**
+ * @brief 构造一个智能指针并返回
+ * 
+ * @tparam T 智能指针存放指针的类型
+ * @param size 数组的大小
+ * @return UniquePointer<T[]> 返回一个智能指针
+ */
 template<typename T>
 UniquePointer<T[]> MakeUnique(int size) {
   return std::move(UniquePointer<T[]>(new T[size]));
