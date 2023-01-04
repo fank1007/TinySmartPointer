4235dca525b30e7a5490606663858a3fb2b3bbda
fk <3445777927@qq.com>
2023-01-04 21:48:58 +0800

    commit

--- source/shared_ptr/shared_ptr.h
+++ source/shared_ptr/shared_ptr.h
@@ -8,7 +8,99 @@ class SharedPointer{
  public:
   SharedPointer() : pointer_(nullptr),user_count_(nullptr) {}
 
-  SharedPointer(T* pointer) : pointer_(pointer),user_count_(new int(1)) {}
+  explicit SharedPointer(T* pointer) : pointer_(pointer),user_count_(new int(1)) {}
+
+  SharedPointer(const SharedPointer& copy)
+      :pointer_(copy.pointer_),
+       user_count_(copy.user_count_) {
+    if (user_count_ != nullptr) {
+      ++(*user_count_);
+    }
+  }
+
+  SharedPointer(SharedPointer&& move)
+      :pointer_(move.pointer_),
+       user_count_(move.user_count_) {
+    move.pointer_ = nullptr;
+    move.user_count_ = nullptr;
+  }
+
+  ~SharedPointer(){
+    if (user_count_ != nullptr) {
+      --(*user_count_);
+    } else if (user_count_ == nullptr || *user_count_ == 0) {
+     delete pointer_;
+     delete user_count_;
+     pointer_ = nullptr;
+     user_count_ = nullptr;
+    }
+  }
+
+  SharedPointer& operator=(const SharedPointer& copy) {
+    if (&copy != this) {
+      pointer_ = copy.pointer_;
+      user_count_ = copy.user_count_;
+      ++(*user_count_);
+      return *this;
+    }
+    return *this;
+  }
+
+  SharedPointer& operator=(SharedPointer&& move) {
+    if (&move != this) {
+      pointer_ = move.pointer_;
+      user_count_ = move.user_count_;
+      move.pointer_ = nullptr;
+      move.user_count_ = nullptr;
+      return *this;
+    }
+    return *this;
+  }
+
+  T* pointer() {
+   return pointer_;
+  }
+
+  int user_count() {
+   return (*user_count);
+  }
+
+  bool Unique() {
+    if (*user_count != 1 || user_count_ == nullptr) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  T operator*() {
+    return *pointer_;
+  }
+
+  T* operator->() {
+    return pointer_;
+  }
+
+  explicit operator bool() {
+    if (pointer_ == nullptr) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  void Reset(T*);
+ private:
+  T* pointer_;
+  int* user_count_;
+};
+
+template<typename T>
+class SharedPointer<T[]>{
+ public:
+  SharedPointer() : pointer_(nullptr),user_count_(nullptr) {}
+
+  explicit SharedPointer(T* pointer) : pointer_(pointer),user_count_(new int(1)) {}
 
   SharedPointer(const SharedPointer& copy)
       :pointer_(copy.pointer_),
@@ -29,8 +121,8 @@ class SharedPointer{
     if (user_count_ != nullptr) {
       --(*user_count_);
     }
-    if (*user_count_ == 0 || user_count_ == nullptr) {
-     delete pointer_;
+    if (user_count_ == nullptr || *user_count_ == 0) {
+     delete[] pointer_;
      delete user_count_;
      pointer_ = nullptr;
      user_count_ = nullptr;
@@ -44,6 +136,7 @@ class SharedPointer{
       ++(*user_count_);
       return *this;
     }
+    return *this;
   }
 
   SharedPointer& operator=(SharedPointer&& move) {
@@ -54,12 +147,84 @@ class SharedPointer{
       move.user_count_ = nullptr;
       return *this;
     }
+    return *this;
   }
-      
+
+  T* pointer() {
+   return pointer_;
+  }
+
+  int user_count() {
+   return (*user_count);
+  }
+
+  bool Unique() {
+    if (*user_count != 1 || user_count_ == nullptr) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  T operator*() {
+    return *pointer_;
+  }
+
+  T operator[](unsigned int index) {
+   return pointer_[index];
+  }
+
+  T* operator->() {
+    return pointer_;
+  }
+
+  explicit operator bool() {
+    if (pointer_ == nullptr) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  void Reset(T*);
+  
  private:
   T* pointer_;
-  unsigned int* user_count_;
+  int* user_count_;
 };
+
+
+template<typename T>
+void SharedPointer<T>::Reset(T* pointer){
+  if (*user_count_ == 1) {
+    auto p = pointer_;
+    pointer_ = pointer;
+    delete p;
+  } else if (user_count_ == nullptr) {
+    pointer_ = pointer;
+    user_count_ = new int(1);
+  } else {
+    pointer_ = pointer;
+    --(*user_count_);
+    user_count_ = new int(1);
+  }
+}
+
+template<typename T>
+void SharedPointer<T[]>::Reset(T* pointer){
+  if (*user_count_ == 1) {
+    auto p = pointer_;
+    pointer_ = pointer;
+    delete[] p;
+  } else if (user_count_ == nullptr) {
+    pointer_ = pointer;
+    user_count_ = new int(1);
+  } else {
+    pointer_ = pointer;
+    --(*user_count_);
+    user_count_ = new int(1);
+  }
+}
 
 } //namespace cpp
 
---
+++ source/shared_ptr/test.cpp
@@ -0,0 +1,10 @@
+#include"shared_ptr.h"
+#include<iostream>
+struct a{
+  a(){std::cout<<"construct"<<std::endl;}
+  ~a(){std::cout<<"destory"<<std::endl;}
+};
+int main(){
+  cpp::SharedPointer<a[]> p(new a[10]);
+  return 0;
+}
